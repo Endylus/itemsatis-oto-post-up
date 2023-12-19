01419ddc24349d45a04e95fe8297bb2b4eb690ba
Endy <122468378+Endylus@users.noreply.github.com>
2023-12-19 23:28:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const fetch = require("node-fetch")
 const { token, data, time } = require("./config.js")
 
 async function main() {
-    data.forEach(async ID => {
-        let newData = await fetch("https://www.itemsatis.com/api/merchant/v1/moveUpMyPost", { "headers": { "content-type": "application/json" }, "body": "{\"postID\":\"" + ID + "\",\"token\":\"" + token + "\"}", "method": "POST" }).then(res => res.json())
-        if (newData.success) {
-            console.log(`İlan ID: ${ID} ${newData.message.replace(/<\/?b>/g, " ").replace(/<br>/g, " ")}`);
-        } else if (newData.success == false) {
-            console.log(`İlan ID: ${ID} ${newData.message.replace(/<\/?b>/g, "").replace(/<br>/g, " ").replace(" Farklı kategoride bulunan ilanlarınızı yukarı taşımayı deneyiniz.", " ")}`);
-        }
+  data.forEach(async ID => {
+    let newData = await fetch("https://www.itemsatis.com/api/moveUpPost", { headers: { "content-type": "application/x-www-form-urlencoded", }, body: `Id=${ID}&token=${token}`, method: "POST" }).then(res => res.json()).catch(err => {
+      console.log(`>> Error: An error occured while fetching data from the API. Please try again later.`);
     });
+    if (newData.success) {
+      console.log(`İlan ID: ${ID} ${newData.message.replace(/<\/?b>/g, " ").replace(/<br>/g, " ")}`);
+    } else if (newData.success == false) {
+      console.log(`İlan ID: ${ID} ${newData.message.replace(/<\/?b>/g, "").replace(/<br>/g, " ").replace(" Farklı kategoride bulunan ilanlarınızı yukarı taşımayı deneyiniz.", " ")}`);
+    }
+  });
 }
 
 main()
