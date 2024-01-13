01959aba7aafa89f72a2f08078422ed5ad47b47f
Endy <122468378+Endylus@users.noreply.github.com>
2024-01-13 09:53:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 module.exports = {
-    token: "", // website token
-    time: 3, // 1.5 saat = 1.5 / 3 saat = 3
-    data: [
-        "1797475",
-        "2040916",
-        "2026070"
-    ] // ilan idleri
+    cookie: '', // Cookie bilgilerinizi girin.
+    postId: ['',], // İlan ID'lerini virgül ile ayırarak yazın. Örnek: ["123456", "654321"]
+    time: 1 // saat cinsinden yazın. Örnek: 1
 }
